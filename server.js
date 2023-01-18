b318831077963c817d54d65e7798a610ed1ec476
Eric Elsner <ericelsnercodes@gmail.com>
2023-01-18 08:49:28 -0500

    server file setup complete

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const express = require('express')
 const morgan = require('morgan') 
 const mongoose = require('mongoose')
 const path = require('path')
+const Drum = require('./models/drum')
 
 // *********** *********** *********** //
 // Database Connection                 //
@@ -25,3 +26,32 @@ mongoose.connection
     .on('close', () => console.log('Disconnected from mongoose'))
     .on('error', (err) => console.log(err))
 
+// *********** *********** *********** //
+// Create Express Application          //
+// *********** *********** *********** //
+
+const app = express()
+
+// *********** *********** *********** //
+// Middleware                          //
+// *********** *********** *********** //
+
+app.use(morgan('tiny')) // logging
+app.use(express({ extended: true })) // parse urlencoded request bodies
+app.use(express.static('public')) // serve files from public statically
+app.use(express.json()) // parses incoming requests with JSON payloads
+
+// *********** *********** *********** //
+// Routes                              //
+// *********** *********** *********** //
+
+app.get('/', (req, res) => {
+    res.send('the server is running')
+})
+
+// *********** *********** *********** //
+// Server Listener                     //
+// *********** *********** *********** //
+
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Now listening on port ${PORT}`))
