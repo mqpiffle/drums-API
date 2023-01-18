34cb200cb0995f0bce422aeb19ee7afac8300dda
Eric Elsner <ericelsnercodes@gmail.com>
2023-01-18 08:21:57 -0500

    database connection initiated

--- server.js
+++ server.js
@@ -8,3 +8,19 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const path = require('path')
 
+// *********** *********** *********** //
+// Database Connection                 //
+// *********** *********** *********** //
+
+const DATABASE_URL = process.env.DATABASE_URL
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect(DATABASE_URL, CONFIG)
+
+mongoose.connection
+    .on('open', () => console.log('Connected to mongoose'))
+    .on('close', () => console.log('Disconnected from mongoose'))
+    .on('error', (err) => console.log(err))
