d295a74177ec033c3d82398cd333ddf51183f4cb
Eric Elsner <ericelsnercodes@gmail.com>
2023-01-18 08:15:00 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+// *********** *********** *********** //
+// Import Dependencies                 //
+// *********** *********** *********** //
+
+require('dotenv').config()
+const express = require('express') 
+const morgan = require('morgan') 
+const mongoose = require('mongoose')
+const path = require('path')
+
